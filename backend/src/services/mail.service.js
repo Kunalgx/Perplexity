6fd5a7ce9d296be23e86dfe49b2c416514501311
Kunalgx <Kunalkumar94937@gmail.com>
2026-09-01 12:37:57 +0530

    format mail service configuration for better readability

--- backend/src/services/mail.service.js
+++ backend/src/services/mail.service.js
@@ -1,14 +1,17 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.GOOGLE_USER,
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    refreshToken: process.env.GOOGLE_REFRESH_TOKEN
-  }
+    service: "gmail",
+
+    auth: {
+        type: "OAuth2",
+        user: process.env.GOOGLE_USER,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN
+    },
+
+    family: 4
 });
 
 transporter.verify((error) => {
