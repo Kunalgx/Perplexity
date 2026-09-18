8bcc7fe3a00bcd58cf5897ced826827308d08514
Kunalgx <Kunalkumar94937@gmail.com>
2026-09-18 20:48:20 +0530

    model problem

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -5,7 +5,7 @@ import *as z from "zod"
 import { searchInternet } from "./internet.service.js";
 
 const geminiModel = new ChatGoogleGenerativeAI({
-  model: "gemini-3.5-Lite",
+  model: "gemini-3.5-medium",
   apiKey: process.env.GEMINI_API_KEY,
 });
 const mistralModel = new ChatMistralAI({
